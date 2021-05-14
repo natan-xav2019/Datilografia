75af3f6de0c35846e4bcc5d4e1e573846f003768
Natan Xavier <natanxaviersiquera@gmail.com>
2021-05-14 18:07:54 -0300

    Adicionado a classe butao variaveis de posição e métodos Gets e Sets restantes

--- src/datilografia/Butao.java
+++ src/datilografia/Butao.java
@@ -6,20 +6,25 @@ public class Butao {
     
     private int alturaButao;
     private int larguraButao;
+    private int posisaoEixoX;
+    private int posisaoEixoY;
     private String textoButao;
     private final JButton botaoInterface = new JButton();
     
-    public Butao(String letra) {
-        setAlturaButao(50);
-        setLarguraButao(50);
+    public Butao(int posisaoEixoX,int posisaoEixoY,int larguraButao,int alturaButao,String letra) {
+        
+        setPosisaoEixoX(posisaoEixoX);
+        setPosisaoEixoY(posisaoEixoY);
+        setLarguraButao(larguraButao);
+        setAlturaButao(alturaButao);
         setLetraButao(letra);
         
-        botaoInterface.setBounds(200,400,getAlturaButao(),getLarguraButao());
+        botaoInterface.setBounds(getPosisaoEixoX(), getPosisaoEixoY(), getLarguraButao(), getAlturaButao());
         
     }
     
     private int getAlturaButao(){
-        return alturaButao;
+        return this.alturaButao;
     }
     
     private void setAlturaButao(int altura){
@@ -27,7 +32,7 @@ public class Butao {
     }
     
     private int getLarguraButao(){
-        return larguraButao;
+        return this.larguraButao;
     }
     
     private void setLarguraButao(int largura){
@@ -35,12 +40,28 @@ public class Butao {
     }
     
     private String getTextoButao(){
-        return textoButao;
+        return this.textoButao;
     }
     
     private void setLetraButao(String letra){
         this.textoButao = letra;
         botaoInterface.setText(getTextoButao());
+    }
+    
+    private int getPosisaoEixoX() {
+        return this.posisaoEixoX; 
+    }
+    
+    private void setPosisaoEixoX(int posisaoX) {
+         this.posisaoEixoX = posisaoX;
+    }
+    
+    private int getPosisaoEixoY() {
+        return this.posisaoEixoY; 
+    }
+    
+    private void setPosisaoEixoY(int posisaoY) {
+         this.posisaoEixoY = posisaoY;
     }
     
     public JButton getButao(){
