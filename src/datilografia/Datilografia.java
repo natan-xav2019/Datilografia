37f16b6d1cce4af2b0a33ce043c7a0fb53ec0e02
Daniel Henrique <danielshenrique@gmail.com>
2021-05-18 15:50:42 -0300

    Adicionado o teclado
    Aqui foi adicionado o teclado a tela principal, porém com isso o quadro fica pequeno demais

--- src/datilografia/Datilografia.java
+++ src/datilografia/Datilografia.java
@@ -4,25 +4,30 @@ Materia: 2019200079, 2019200296
  */
 package datilografia;
 
+import java.awt.FlowLayout;
 import javax.swing.*;
 
 public class Datilografia {
 
     public static void main(String[] args) {
 
-        
         // Como está escrito no documento, to pensando transformar tudo isso em uma classe
         // Para que possamos gerenciar isso melhor sem deixar o main bagunçado
         JFrame janela = new JFrame("Datilografia");
-        JFrame b = new JFrame("a");//sintaxe do butao(posição na tela x,posição na tela y, tamanho do butao x,tamanho do butao y, conteudo do botao );
-        
+        Teclado teclado = new Teclado();
+
         QuadroNegro t1 = new QuadroNegro();
         janela.add(t1.getQuadroNegro());
 
-        janela.add(b);
+        teclado.criarTeclado(janela);
+
+        for (JButton botoes : teclado.getBotoes()) {
+            janela.add(botoes);
+
+        }
 
         janela.setSize(1100, 800);
-        janela.setLayout(null);
+        janela.setLayout(new FlowLayout());
         janela.setLocationRelativeTo(null);//Com a possição relativa sendo null nao precisamos colotar posição relativa nos elementos 
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);
