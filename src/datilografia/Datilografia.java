cd65ed590f2152d7b029d1a0b92d3b1a55ea14ef
Natan Xavier <natanxaviersiquera@gmail.com>
2021-05-18 11:49:38 -0300

    removendo Comentarios

--- src/datilografia/Datilografia.java
+++ src/datilografia/Datilografia.java
@@ -14,34 +14,18 @@ public class Datilografia {
         // Como está escrito no documento, to pensando transformar tudo isso em uma classe
         // Para que possamos gerenciar isso melhor sem deixar o main bagunçado
         JFrame janela = new JFrame("Datilografia");
-        Botao b = new Botao(200, 400, 50, 50, "a");//sintaxe do butao(posição na tela x,posição na tela y, tamanho do butao x,tamanho do butao y, conteudo do botao );
-
+        JFrame b = new JFrame("a");//sintaxe do butao(posição na tela x,posição na tela y, tamanho do butao x,tamanho do butao y, conteudo do botao );
+        
         QuadroNegro t1 = new QuadroNegro();
         janela.add(t1.getQuadroNegro());
 
-        janela.add(b.getButao());
+        janela.add(b);
 
         janela.setSize(1100, 800);
         janela.setLayout(null);
-        janela.setLocationRelativeTo(null);
+        janela.setLocationRelativeTo(null);//Com a possição relativa sendo null nao precisamos colotar posição relativa nos elementos 
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);
-
-        /*
-        Teste meio falho, mas da para usar o FlowLayout para alinhar as coisas
-        
-        Container painel = janela.getContentPane();
-        painel.setLayout(new FlowLayout());
-
-        JButton a = new JButton("a");
-        painel.add(a);
-        a = new JButton("b");
-        painel.add(a);
-        a = new JButton("c");
-        painel.add(a);
-        a = new JButton("d");
-        painel.add(a);
-        */
     }
 
 }
