6a15cff3d8ec0da34da4131fac766cb64635cc58
Daniel Henrique <danielshenrique@gmail.com>
2021-05-19 10:59:05 -0300

    Removendo coisas não utilizadas
    Tirando referência não utilizadas na classe do teclado, porém essas coisas podem voltar no futuro.

--- src/datilografia/Teclado.java
+++ src/datilografia/Teclado.java
@@ -1,32 +1,19 @@
 //Quantidade de teclas 14+14+13+12+4 = 57
 package datilografia;
 
-import java.awt.FlowLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import java.util.ArrayList;
 
 public class Teclado {
     //Primeiramente precisamos de Jframe. Depois vamos precisar de um container e depois o FlowLayout.
     ArrayList<JButton> botoes = new ArrayList<>();
-    private char alfabeto = 'a';
     
-    public void criarTeclado(JFrame jframe){
-        jframe = new JFrame();
-        jframe.setLayout(new FlowLayout());
-        
-        String textoBotao;
-        
+    public void criarTeclado(){
+       
         for (int indice = 0, alfabeto ='a' ; indice < 26; indice++) {
             
-            textoBotao = Integer.toString(indice);
             botoes.add(new JButton( Character.toString(alfabeto++) ) );
-        }
-        
-        /*for (JButton botao : botoes){
-            jframe.add(botao);
-        Comentei esta linha pois nao estava fazendo diferença no codigo
-        }*/ 
+        } 
         
     }
 
