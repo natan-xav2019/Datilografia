26580625df584fdc6a93958d80e6e718e77cd7ee
Daniel Henrique <danielshenrique@gmail.com>
2021-05-24 11:04:54 -0300

    Reorganizado a classe teclado

--- src/datilografia/Teclado.java
+++ src/datilografia/Teclado.java
@@ -7,28 +7,28 @@ import javax.swing.JButton;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
-
 public class Teclado {
 
-    private ArrayList<JButton> botoes = new ArrayList<>();
-    private JPanel painel = new JPanel();
+    private final ArrayList<JButton> botoes = new ArrayList<>(); //Agora fiquei em dúvida, pode ser que a gente tenha que usar uma classe botão mesmo
+    private final JPanel painel = new JPanel();
 
     private char alfabeto = 'A';
 
     public Teclado() {
 
         painel.setLayout(new FlowLayout());//layout flexivel
-        painel.setPreferredSize(new Dimension(500,500));//dimensao do painel
-        
+        painel.setPreferredSize(new Dimension(500, 500));//dimensao do painel
+
         for (int indice = 0; indice < 26; indice++) {//for coloca as letras do alfabeto
             botoes.add(new JButton(Character.toString(this.alfabeto++)));// as letras do alfabeto
-            botoes.get(indice).setPreferredSize(new Dimension(50,40));//Dimensação do botao
+            botoes.get(indice).setPreferredSize(new Dimension(50, 40));//Dimensação do botao
         }
 
         for (JButton botao : botoes) { //adiciona os botoes no painel
             painel.add(botao);
         }
 
+        //precisamos botar os botões no padrão de um teclado normal
     }
 
     public JPanel getPainel() {//pegar painel
