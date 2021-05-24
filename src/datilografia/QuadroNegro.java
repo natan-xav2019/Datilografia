1c1ff8196c4fac799444bee8e8ee45c09ded6e75
Daniel Henrique <danielshenrique@gmail.com>
2021-05-24 11:03:48 -0300

    Função de quebrar linha

--- src/datilografia/QuadroNegro.java
+++ src/datilografia/QuadroNegro.java
@@ -1,4 +1,3 @@
-
 package datilografia;
 
 import java.awt.BorderLayout;
@@ -7,13 +6,16 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class QuadroNegro {
-    
-    JPanel painel = new JPanel();
-    
+
+    private final JPanel painel = new JPanel();
     private final JTextArea quadro = new JTextArea();
-    
+
     public QuadroNegro() { //Contrutor do QuadroNegro
-        quadro.setPreferredSize(new Dimension(500,300));
+        //quadro.setPreferredSize(new Dimension(500, 300));
+        quadro.setLineWrap(true); //Faz a quebra de linha 
+        //quadro.setWrapStyleWord(true); AINDA NÃO SEI O QUE FAZ DIREITO, MAS VAMOS TESTANDO
+
+        //Ha Função de definir linhas e colunas, o que acha de botarmos ela?
         
         painel.setLayout(new BorderLayout());
         painel.add(quadro);
