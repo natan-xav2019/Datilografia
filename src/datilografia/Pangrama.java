9e6322689133e0bebb7cc39657cb72c1045e0fc5
Daniel Henrique <danielshenrique@gmail.com>
2021-05-25 11:19:49 -0300

    Criado a classe pangrama
    Ela serve para armazenar os pangramas e fazer suas operações

---
+++ src/datilografia/Pangrama.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datilografia;
+import java.util.ArrayList;
+/**
+ *
+ * @author Administrador
+ */
+public class Pangrama {
+    private final ArrayList<String> pangrama = new ArrayList<String>();
+    
+    //Aqui a gente pode fazer um método para colocar os pangramas nesse arraylist
+    
+    //Também podemos fazer aqui um método para comparar se está certo ou não 
+}
