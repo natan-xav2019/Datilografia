4e0fb846143d19fee7d443e106c78cd3aea74324
Daniel Henrique <danielshenrique@gmail.com>
2021-05-27 00:00:01 -0300

    Nova Classe Pangrama
    Será usada tanto para armazenar os pangramas, quanto para exibi-los, comparar e fazer as operações com os pangramas.

--- src/datilografia/Pangrama.java
+++ src/datilografia/Pangrama.java
@@ -5,14 +5,34 @@
  */
 package datilografia;
 import java.util.ArrayList;
+import javax.swing.*;
+
 /**
  *
  * @author Administrador
  */
 public class Pangrama {
+    
+    private final JLabel pergunta = new JLabel(); //Será que essa é a melhor maneira?
     private final ArrayList<String> pangrama = new ArrayList<String>();
+    
     
     //Aqui a gente pode fazer um método para colocar os pangramas nesse arraylist
     
     //Também podemos fazer aqui um método para comparar se está certo ou não 
+
+    public void setPangrama(String pangrama){
+        //Temos que fazer uma excessão para saber se a frase é realmente um pangrma
+        this.pangrama.add(pangrama);
+    }
+
+    public JLabel getPergunta() {
+        //Teste, precisamos melhorar isso depois
+        pergunta.setText(pangrama.get(0));
+               
+        //Podemos criar aqui uma variável para mudar os index da pergunta
+        return pergunta;
+    }
+    
+
 }
