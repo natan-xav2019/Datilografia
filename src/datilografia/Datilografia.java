67ca11b6d7769bc4a7a3ed744d467451363b1d2e
Daniel Henrique <danielshenrique@gmail.com>
2021-05-30 17:19:26 -0300

    Mudança para poder usar operações de listening

--- src/datilografia/Datilografia.java
+++ src/datilografia/Datilografia.java
@@ -14,23 +14,22 @@ public class Datilografia {
 
         Janela janela = new Janela("Datilografia");
         JPanel painel = new JPanel();
-        //JLabel texto = new JLabel();  //Vai querer usar esse aqui para as perguntas ? Se sim, teremos que criar uma classe
         Pangrama pangrama = new Pangrama();
         Teclado teclado = new Teclado();
         QuadroNegro t1 = new QuadroNegro();
 
-        //texto.setText("Ola mundo!!");//texto para ver como o Jlabel iria se comportar
-        pangrama.setPangrama("OI");
         
         painel.setLayout(new BorderLayout());
-        //painel.add(texto, BorderLayout.PAGE_START);
         painel.add(pangrama.getPergunta(), BorderLayout.PAGE_START);
         painel.add(t1.getQuadroNegro(), BorderLayout.CENTER);
         painel.add(teclado.getPainel(), BorderLayout.PAGE_END);
 
         janela.getJanela().add(painel);
+        
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            janela.painel();
+        });
 
-        janela.painel();
     }
 
 }
