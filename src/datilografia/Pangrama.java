7513b44339d76cbde1d31e921cc588aa220c0c07
Daniel Henrique <danielshenrique@gmail.com>
2021-05-30 17:20:31 -0300

    Adicionado frases na classe pangrama
    retirado o método setpangrama e colocado algumas frases do pangrama

--- src/datilografia/Pangrama.java
+++ src/datilografia/Pangrama.java
@@ -17,19 +17,21 @@ public class Pangrama {
     private final ArrayList<String> pangrama = new ArrayList<String>();
     
     
+    public Pangrama(){
+        pangrama.add("Gazeta publica hoje breve anúncio de faxina na quermesse");
+        pangrama.add("The quick brown fox jumps over de lazy dog");
+        pangrama.add("Quick wafting zephyrs vex bold Jim");
+        
+    }
+    
     //Aqui a gente pode fazer um método para colocar os pangramas nesse arraylist
     
     //Também podemos fazer aqui um método para comparar se está certo ou não 
 
-    public void setPangrama(String pangrama){
-        //Temos que fazer uma excessão para saber se a frase é realmente um pangrma
-        this.pangrama.add(pangrama);
-    }
-
     public JLabel getPergunta() {
         //Teste, precisamos melhorar isso depois
         pergunta.setText(pangrama.get(0));
-               
+        
         //Podemos criar aqui uma variável para mudar os index da pergunta
         return pergunta;
     }
