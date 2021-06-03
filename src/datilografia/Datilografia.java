d6698945a42a10cabf7b35cb6c35dd0325630f92
Natan Xavier <natanxaviersiquera@gmail.com>
2021-06-02 21:18:24 -0300

    Trocando nome de t1 para quadroNegro

--- src/datilografia/Datilografia.java
+++ src/datilografia/Datilografia.java
@@ -5,26 +5,30 @@ Materia: 2019200079, 2019200296
 package datilografia;
 
 import java.awt.BorderLayout;
-//import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Datilografia {
-
+    
     public static void main(String[] args) {
 
         Janela janela = new Janela("Datilografia");
         JPanel painel = new JPanel();
         Pangrama pangrama = new Pangrama();
         Teclado teclado = new Teclado();
-        QuadroNegro t1 = new QuadroNegro();
-
+        QuadroNegro quadroNegro = new QuadroNegro();
         
+        EventosDatilografia Evento = new EventosDatilografia(teclado);
+
+            
         painel.setLayout(new BorderLayout());
-        painel.add(pangrama.getPergunta(), BorderLayout.PAGE_START);
-        painel.add(t1.getQuadroNegro(), BorderLayout.CENTER);
-        painel.add(teclado.getPainel(), BorderLayout.PAGE_END);
+        
+        painel.add(pangrama.getPangrama(), BorderLayout.PAGE_START);
+        painel.add(quadroNegro.getQuadroNegro(), BorderLayout.CENTER);
+        painel.add(teclado.getTeclado(), BorderLayout.PAGE_END);
 
         janela.getJanela().add(painel);
+        
+        quadroNegro.getJTextArea().addKeyListener( Evento );
         
         javax.swing.SwingUtilities.invokeLater(() -> {
             janela.painel();
