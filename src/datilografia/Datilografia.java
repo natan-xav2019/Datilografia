5941c66750237db7e347b6bd3a223923314e13bf
Daniel Henrique <danielshenrique@gmail.com>
2021-06-03 16:38:39 -0300

    Mudança de indentação

--- src/datilografia/Datilografia.java
+++ src/datilografia/Datilografia.java
@@ -8,7 +8,7 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 
 public class Datilografia {
-    
+
     public static void main(String[] args) {
 
         Janela janela = new Janela("Datilografia");
@@ -16,23 +16,22 @@ public class Datilografia {
         Pangrama pangrama = new Pangrama();
         Teclado teclado = new Teclado();
         QuadroNegro quadroNegro = new QuadroNegro();
-        
-        EventosDatilografia Evento = new EventosDatilografia(teclado,quadroNegro,pangrama);
-        
-        quadroNegro.getJTextArea().addKeyListener( Evento );
-            
+
+        EventosDatilografia Evento = new EventosDatilografia(teclado, quadroNegro, pangrama);
+
+        quadroNegro.getJTextArea().addKeyListener(Evento);
+
         painel.setLayout(new BorderLayout());
-        
+
         painel.add(pangrama.getPangrama(), BorderLayout.PAGE_START);
         painel.add(quadroNegro.getQuadroNegro(), BorderLayout.CENTER);
         painel.add(teclado.getTeclado(), BorderLayout.PAGE_END);
 
         janela.getJanela().add(painel);
-        
+
         javax.swing.SwingUtilities.invokeLater(() -> {
             janela.painel();
         });
 
     }
-
 }
