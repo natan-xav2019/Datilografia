00f9c76177331147be1759b91281677ceccbcf2d
Natan Xavier <natanxaviersiquera@gmail.com>
2021-06-02 21:21:10 -0300

    Datilografia Proposta de Pangrama

--- src/datilografia/Pangrama.java
+++ src/datilografia/Pangrama.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package datilografia;
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 import javax.swing.*;
 
@@ -13,28 +14,46 @@ import javax.swing.*;
  */
 public class Pangrama {
     
+    private final JPanel painel = new JPanel();
     private final JLabel pergunta = new JLabel(); //Será que essa é a melhor maneira?
+    private final JLabel Instrusao = new JLabel("Instrução de como funciona");
     private final ArrayList<String> pangrama = new ArrayList<String>();
     
+    private int indise = 0;
     
     public Pangrama(){
+        painel.setLayout(new BorderLayout());
+        
+        
         pangrama.add("Gazeta publica hoje breve anúncio de faxina na quermesse");
         pangrama.add("The quick brown fox jumps over de lazy dog");
         pangrama.add("Quick wafting zephyrs vex bold Jim");
         
+        painel.add(Instrusao);
+        painel.add(getPergunta());
+
     }
     
     //Aqui a gente pode fazer um método para colocar os pangramas nesse arraylist
     
     //Também podemos fazer aqui um método para comparar se está certo ou não 
 
-    public JLabel getPergunta() {
-        //Teste, precisamos melhorar isso depois
-        pergunta.setText(pangrama.get(0));
+    private JLabel getPergunta() {
+        //pode se colocar sempre na mesma ordem assim a nossa organização fica mais facil para a correção das mesmas frazes pois os idices seram os mesmo
+        pergunta.setText(pangrama.get(indise));
         
-        //Podemos criar aqui uma variável para mudar os index da pergunta
+        //a cada chamada o index pode somar 1 botei numa função que se quiser incrementar algo e so mudar ela
+        proximaPergunta();
+        
         return pergunta;
     }
     
+    public JPanel getPangrama(){
+        return painel;
+    }
+    
+    public void proximaPergunta(){
+        indise++;
+    }
 
 }
