ab8a2b2ba156a135eea1de9ae9685bbf121d0ab6
Daniel Henrique <danielshenrique@gmail.com>
2021-06-03 17:46:20 -0300

    Indentação no Pangrama

--- src/datilografia/Pangrama.java
+++ src/datilografia/Pangrama.java
@@ -9,8 +9,8 @@ public class Pangrama {
 
     private final JPanel painel = new JPanel();
 
-    private final JLabel pergunta = new JLabel(); //Será que essa é a melhor maneira?
-    private final JLabel Instrusao = new JLabel("Instrução de como funciona");
+    private final JLabel pergunta = new JLabel();
+    private final JLabel instrucao = new JLabel("Instrução de como funciona");
     private final ArrayList<String> pangrama = new ArrayList<String>();
 
     private int indice = 0;
@@ -18,21 +18,19 @@ public class Pangrama {
     public Pangrama() {
         painel.setLayout(new BorderLayout());
 
-        pergunta.setFont(new Font("Ariel",Font.PLAIN,30));
-        
+        pergunta.setFont(new Font("Ariel", Font.PLAIN, 30));
+
         pangrama.add("Gazeta publica hoje breve anúncio de faxina na quermesse\n");
         pangrama.add("The quick brown fox jumps over de lazy dog\n");
         pangrama.add("Quick wafting zephyrs vex bold Jim\n");
-   
+
         painel.add(getPergunta());
 
     }
 
-
     private JLabel getPergunta() {
-        
-        pergunta.setText(pangrama.get(indise));
-        
+
+        pergunta.setText(pangrama.get(indice));
 
         return pergunta;
     }
@@ -44,9 +42,11 @@ public class Pangrama {
     public ArrayList<String> getFrase() {
         return pangrama;
     }
-    
-    public void proximaPergunta(){
-        indise++;
-        pergunta.setText(pangrama.get(indise));
+
+    public void proximaPergunta() {
+
+        indice++;
+        pergunta.setText(pangrama.get(indice));
+
     }
 }
