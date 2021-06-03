8087c11dd9cf60bd7e5dc9d13938737427cf751f
Daniel Henrique <danielshenrique@gmail.com>
2021-06-03 16:40:19 -0300

    Indentação de Quadro Negro

--- src/datilografia/QuadroNegro.java
+++ src/datilografia/QuadroNegro.java
@@ -5,34 +5,24 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-
-
-public class QuadroNegro{
-
+public class QuadroNegro {
 
     private final JPanel painel = new JPanel();
     private final JTextArea quadro = new JTextArea();
     private final JScrollPane area = new JScrollPane(quadro);
 
-    
-
     public QuadroNegro() { //Contrutor do QuadroNegro
-        quadro.setLineWrap(true); //Faz a quebra de linha 
-              
-        //System.out.println(quadro.getText());  //Teste de como pegar o texto que esta escrito na tela
+        quadro.setLineWrap(true); //Faz a quebra de linha
 
         painel.setLayout(new BorderLayout());
         painel.add(area);
-        
-        //não esta aceitando funções publicas
     }
-    
-    public JPanel getQuadroNegro() { //Aqui retornamos o Objeto JTextArea Tela
+
+    public JPanel getQuadroNegro() { //Aqui retornamos o Objeto JTextArea
         return painel;
     }
 
-    
-    public JTextArea getJTextArea() { //Aqui retornamos o Objeto JTextArea Tela
+    public JTextArea getJTextArea() { //Aqui retornamos o Objeto JTextArea
         return quadro;
     }
 }
